0e16ff5dec6c985508ce0fc7e7c2bae67ea49ff2
Callum McCracken <callum189@gmail.com>
2024-12-22 12:06:05 +0100

    ensure best-fit parameters are not negative

--- fitting.py
+++ fitting.py
@@ -9,7 +9,9 @@ def find_best_fit_params(times: np.ndarray, intensities: np.ndarray,
     Given time and intensity values, find k_2_conc_a, k_3, k_4, and k_ph.
     """
 
-    result = curve_fit(triplet_decay_solution(epsilon), times, intensities, p0=initial_guesses)
+    result = curve_fit(
+        triplet_decay_solution(epsilon), times, intensities, p0=initial_guesses,
+        bounds=([0, 0, 0, 0], [np.inf, np.inf, np.inf, np.inf]))
     popt = result[0]
     k_2_conc_a, k_3, k_4, k_ph = popt
 
